94ce26506b5c052a98968fdf4c362240e81d0f27
MykhailoArtiukh <artiukh.misha@gmail.com>
2025-03-17 13:28:51 +0100

    output db, footer to the bottom

--- Restaurant/inventory/views.py
+++ Restaurant/inventory/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Purchase, MenuItem, Ingridient, RecipeRequirement
 # Create your views here.
 
 
@@ -8,12 +8,28 @@ def home(request):
 
 
 def inventory(request):
-    return render(request, "inventory/inventory.html")
+    return render(
+        request,
+        "inventory/inventory.html",
+        context={"ingridients": Ingridient.objects.all()},
+    )
 
 
 def menu(request):
-    return render(request, "inventory/menu.html")
+    return render(
+        request,
+        "inventory/menu.html",
+        context={"menu_items": MenuItem.objects.all()},
+    )
 
 
 def purchase(request):
-    return render(request, "inventory/purchases.html")
+    menu_items = {item.id: item.name for item in MenuItem.objects.all()}
+    return render(
+        request,
+        "inventory/purchases.html",
+        context={
+            "purchases": Purchase.objects.all(),
+            "menu_items": MenuItem.objects.all(),
+        },
+    )
